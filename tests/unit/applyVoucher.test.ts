04ed8e505618d07d11448dab93452aab78352479
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-03-17 18:48:46 -0300

    tests: completed full test suite for apply voucher and related functions

--- tests/unit/applyVoucher.test.ts
+++ tests/unit/applyVoucher.test.ts
@@ -4,8 +4,8 @@ import { Voucher } from '@prisma/client';
 import voucherRepository from 'repositories/voucherRepository';
 import voucherService from 'services/voucherService';
 
-describe("changeVoucherToUsed test suite", () => {
-  it("should resolve async function and return Voucher if code exists", () => {
+describe('changeVoucherToUsed test suite', () => {
+  it('should resolve async function and return Voucher if code exists', () => {
     expect(async () => {
       const mockVoucher = {
         id: 1,
@@ -13,38 +13,38 @@ describe("changeVoucherToUsed test suite", () => {
         discount: 70,
         used: true
       };
-      jest.spyOn(voucherRepository, "useVoucher").mockResolvedValueOnce(mockVoucher);
+      jest.spyOn(voucherRepository, 'useVoucher').mockResolvedValueOnce(mockVoucher);
   
       const result = await voucherService.changeVoucherToUsed('12345678');
       expect(result).toMatchObject(mockVoucher);
     }).resolves;
   });
 
-  it("should reject async function code does not exist", () => {
+  it('should reject async function code does not exist', () => {
     expect(async () => {
-      jest.spyOn(voucherRepository, "useVoucher").mockRejectedValueOnce(undefined);
+      jest.spyOn(voucherRepository, 'useVoucher').mockRejectedValueOnce(undefined);
   
       await voucherService.changeVoucherToUsed('12345678');
     }).rejects;
   });
 });
 
-describe("isAmountValidForDiscount test suite", () => {
-  it("should return false if amount is lower than minimum", () => {
+describe('isAmountValidForDiscount test suite', () => {
+  it('should return false if amount is lower than minimum', () => {
     const min = voucherService.MIN_VALUE_FOR_DISCOUNT;
 
     const result = voucherService.isAmountValidForDiscount(min-1);
     expect(result).toBe(false);
   });
 
-  it("should return true if amount equals minimum", () => {
+  it('should return true if amount equals minimum', () => {
     const min = voucherService.MIN_VALUE_FOR_DISCOUNT;
 
     const result = voucherService.isAmountValidForDiscount(min);
     expect(result).toBe(true);
   });
 
-  it("should return true if amount is greater than minimum", () => {
+  it('should return true if amount is greater than minimum', () => {
     const min = voucherService.MIN_VALUE_FOR_DISCOUNT;
 
     const result = voucherService.isAmountValidForDiscount(min+1);
@@ -52,79 +52,89 @@ describe("isAmountValidForDiscount test suite", () => {
   });
 });
 
-describe("applyDiscount test suite", () => {
-  it("should return 30% of value in case of 70% discount", () => {
+describe('applyDiscount test suite', () => {
+  it('should return 30% of value in case of 70% discount', () => {
     const result = voucherService.applyDiscount(200, 70);
     expect(result).toBe(60);
   });
 
-  it("should return 100% of value in case of 0% discount", () => {
+  it('should return 100% of value in case of 0% discount', () => {
     const result = voucherService.applyDiscount(200, 0);
     expect(result).toBe(200);
   });
 
-  it("should return 0 value in case of 100% discount", () => {
+  it('should return 0 value in case of 100% discount', () => {
     const result = voucherService.applyDiscount(200, 100);
     expect(result).toBe(0);
   });
 });
 
-
-
-
-
-
-
-
-describe("applyVoucher test suite", () => {
-  it("should throw conflict error if there is not a voucher with this code", async () => {
+describe('applyVoucher test suite', () => {
+  it('should throw conflict error if there is not a voucher with this code', async () => {
     expect(async () => {
-      jest.spyOn(voucherRepository, "getVoucherByCode").mockResolvedValue(undefined);
+      jest.spyOn(voucherRepository, 'getVoucherByCode').mockResolvedValue(undefined);
 
       await voucherService.applyVoucher('12345678', 200);
     }).rejects.toMatchObject({ type: 'conflict', message: 'Voucher does not exist.' });
   });
 
+  it('should not apply voucher if it is used and amount is not valid', async () => {
+      const mockVoucher: Voucher = {
+        id: 1,
+        code: '12345678',
+        discount: 70,
+        used: true,
+      };
+      jest.spyOn(voucherRepository, 'getVoucherByCode').mockResolvedValue(mockVoucher);
+      jest.spyOn(voucherService, 'isAmountValidForDiscount').mockImplementationOnce((amount) => { return false; })
 
-
-
-
-    it("should", async () => {
-      expect(async () => {
-        const mockVoucher: Voucher = {
-          id: 1,
-          code: '12345678',
-          discount: 70,
-          used: false,
-        }
-        jest.spyOn(voucherRepository, "getVoucherByCode").mockResolvedValue(mockVoucher);
-  
-        await voucherService.applyVoucher('12345678', 200);
-      }).rejects.toMatchObject({ type: 'conflict', message: 'Voucher does not exist.' });
+      const result = await voucherService.applyVoucher('12345678', 200);
+      expect(result).toMatchObject({
+        amount: 200,
+        discount: mockVoucher.discount,
+        finalAmount: 200,
+        applied: false
+      });
     });
 
+  it('should not apply voucher if it is used', async () => {
+    const mockVoucher: Voucher = {
+      id: 1,
+      code: '12345678',
+      discount: 70,
+      used: true,
+    };
+    jest.spyOn(voucherRepository, 'getVoucherByCode').mockResolvedValue(mockVoucher);
+    jest.spyOn(voucherService, 'isAmountValidForDiscount').mockImplementationOnce((amount) => { return true; })
 
+    const result = await voucherService.applyVoucher('12345678', 200);
+    expect(result).toMatchObject({
+      amount: 200,
+      discount: mockVoucher.discount,
+      finalAmount: 200,
+      applied: false
+    });
+  });
 
+  it('should apply voucher and return discounts', async () => {
+    const mockVoucher: Voucher = {
+      id: 1,
+      code: '12345678',
+      discount: 70,
+      used: false,
+    };
+    jest.spyOn(voucherRepository, 'getVoucherByCode').mockResolvedValue(mockVoucher);
+    jest.spyOn(voucherService, 'isAmountValidForDiscount').mockImplementationOnce((amount) => { return true; });
+    jest.spyOn(voucherRepository, 'useVoucher').mockResolvedValueOnce(undefined);
+    jest.spyOn(voucherService, 'changeVoucherToUsed').mockResolvedValue({ used: true, ...mockVoucher });
+    jest.spyOn(voucherService, 'applyDiscount').mockReturnValueOnce(60);
 
-
-
-
-
-
-
-
-
-
-
-
-
-  it("should resolve the async function if there is no conflict", async () => {
-    expect(async () => {
-      jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => { return undefined; });
-
-      jest.spyOn(voucherRepository, "createVoucher").mockImplementationOnce((): any => { return true; });
-  
-      await voucherService.createVoucher('12345678', 70);
-    }).resolves;
-  });  
+    const result = await voucherService.applyVoucher('12345678', 200);
+    expect(result).toMatchObject({
+      amount: 200,
+      discount: mockVoucher.discount,
+      finalAmount: 60,
+      applied: true
+    });
+  });
 });
--- tests/unit/createVoucher.test.ts
+++ tests/unit/createVoucher.test.ts
@@ -4,8 +4,8 @@ import { Voucher } from '@prisma/client';
 import voucherRepository from 'repositories/voucherRepository';
 import voucherService from 'services/voucherService';
 
-describe("createVoucher test suite", () => {
-  it("should throw conflict error if there is already a voucher with this code", async () => {
+describe('createVoucher test suite', () => {
+  it('should throw conflict error if there is already a voucher with this code', async () => {
     expect(async () => {
       const mockVoucher: Voucher = {
         id: 1,
@@ -14,17 +14,17 @@ describe("createVoucher test suite", () => {
         used: false,
       }
   
-      jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => { return mockVoucher; });
+      jest.spyOn(voucherRepository, 'getVoucherByCode').mockImplementationOnce((): any => { return mockVoucher; });
 
       await voucherService.createVoucher(mockVoucher.code, mockVoucher.discount);
     }).rejects.toMatchObject({ type: 'conflict', message: 'Voucher already exist.' });
   });
 
-  it("should resolve the async function if there is no conflict", async () => {
+  it('should resolve the async function if there is no conflict', async () => {
     expect(async () => {
-      jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => { return undefined; });
+      jest.spyOn(voucherRepository, 'getVoucherByCode').mockImplementationOnce((): any => { return undefined; });
 
-      jest.spyOn(voucherRepository, "createVoucher").mockImplementationOnce((): any => { return true; });
+      jest.spyOn(voucherRepository, 'createVoucher').mockImplementationOnce((): any => { return true; });
   
       await voucherService.createVoucher('12345678', 70);
     }).resolves;
